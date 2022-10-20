5158061e22de0a6e267b6a57e71078534651031c
jayhun0521 <jayhun21@gmail.com>
2022-10-20 09:09:48 -0400

    new push

---
+++ oop.js
@@ -0,0 +1,41 @@
+// const myArray = 
+// [["Michael Jordan", 23], ["Scottie Pippen", 33], 
+// [["Lebron James", 6], ["Kobe Bryant", 8]]
+// ];
+// console.log(myArray);
+
+
+// let sports = [
+// ["Basketball", "Football", "Baseball"],
+// ["Hockey", "Soccer", "Lacrosse"],
+// ["Water Polo", "Competitive Diving", "400m Breast Stroke"]
+// ];
+//   for(let i= 0; i < sports.length; i++){
+//     for(let j = 0; j < sports[i].length; j++)
+//     console.log(i, j, sports[i][j]);
+//   }
+
+
+// const arrayOne = [52,43];
+// console.log(Math.max(...arrayOne));
+
+
+// function add(a, b) {
+//     if(a > b){
+//         return "First number is greater";
+//     }else{
+//         return "Equal than or less than";
+//     }
+// }
+// console.log(add(10, 6));
+
+// function equality(a,b)
+// {
+//   if(a === b){
+//     return "Equal";
+//   }  else{
+//     return "Not equal";
+//   }
+// }
+// console.log(equality(20, "20"));
+
